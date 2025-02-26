3c19913acf9c292dea9c17440f315cafc8e14c3d
N3trunneR404 <shubhuashu@gmail.com>
2025-02-26 05:51:00 +0530

    Progress update: Exceptions.py

---
+++ Compiler/Exceptions.py
@@ -0,0 +1,14 @@
+# Custom exception for syntax errors in the byteflow compiler
+class BFSyntaxError(Exception):
+    # The 'pass' statement is used here to indicate that no additional
+    # functionality or attributes are being added to this custom exception.
+    # It serves as a placeholder to define the exception class.
+    pass
+
+
+# Custom exception for semantic errors in the byteflow compiler
+class BFSemanticError(Exception):
+    # The 'pass' statement is used here to indicate that no additional
+    # functionality or attributes are being added to this custom exception.
+    # It serves as a placeholder to define the exception class.
+    pass
